967a59e903b78b62d74ba4dbf4c98302bdbf9860
d.prushchak <d.prushchak@adbakers.com.ua>
2019-01-31 15:39:31 +0200

    front start

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+        <ul class="header-list">
+            <li class="header-item"
+                @click="doSomething()"
+            >
+                Сама собі зірка
+            </li>
+            <li class="header-item">
+                Призи
+            </li>
+            <li class="header-item">
+                Як взяти участь
+            </li>
+            <li class="header-item">
+                Галерея
+            </li>
+            <li class="header-item">
+                Правила
+            </li>
+            <li class="header-item">
+                Особистий кабінет
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        doSomething() {
+            console.log('click');
+        }
+    },
+}
+</script>
