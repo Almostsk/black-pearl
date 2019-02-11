d8d08d98787a3b946405aecd5f8e5daab895f743
d.prushchak <d.prushchak@adbakers.com.ua>
2019-02-11 14:07:13 +0200

    front almost done

---
+++ resources/js/components/RegisterStar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="register-star">
+        <div class="register-star-title-container">
+            <h1 class="register-star-title">БАЖАЄТЕ СТАТИ ЗІРКОЮ РЕКЛАМНОЇ КАМПАНІЇ «ЧЕРНЫЙ ЖЕМЧУГ»? </h1>
+            <span>Тоді візьміть участь у творчому конкурсі «Сама собі зірка»</span>
+        </div>
+        <div class="register-star-form">
+            <div class="register-quote">
+                <span class="register-quote-text">Розкажіть про свою сцену чи аудиторію, <br>
+                    що Вас надихає бути привабливою
+                </span>
+            </div>
+            <div class="register-photo">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,22 @@
+import Page404 from './components/404.vue';
 import Home from './components/Home.vue';
+import Cabinet from './components/Cabinet.vue';
+import Gallery from './components/Gallery.vue';
+import Register from './components/Register.vue';
 import Login from './components/Login.vue';
+import Feedback from './components/Feedback.vue';
+import RulesPopup from './components/RulesPopup.vue';
+import Rules from './components/Rules.vue';
 
 export const routes = [
+    { 
+        path: '/404', 
+        component: Page404, 
+        name: '404',
+        meta:{
+            web:true
+        } 
+    },
     { 
         path: '/', 
         component: Home, 
@@ -11,9 +26,57 @@ export const routes = [
         } 
     },
     { 
+        path: '/cabinet', 
+        component: Cabinet, 
+        name: 'Cabinet',
+        meta:{
+            web:true
+        } 
+    },
+    { 
+        path: '/gallery', 
+        component: Gallery, 
+        name: 'Gallery',
+        meta:{
+            web:true
+        } 
+    },
+    { 
+        path: '/registration', 
+        component: Register, 
+        name: 'Register',
+        meta:{
+            web:true
+        } 
+    },
+    { 
         path: '/login', 
         component: Login, 
         name: 'Login',
+        meta:{
+            web:true
+        } 
+    },
+    { 
+        path: '/feedback', 
+        component: Feedback, 
+        name: 'Feedback',
+        meta:{
+            web:true
+        } 
+    },
+    { 
+        path: '/rules-popup', 
+        component: RulesPopup, 
+        name: 'RulesPopup',
+        meta:{
+            web:true
+        } 
+    },
+    { 
+        path: '/rules', 
+        component: Rules, 
+        name: 'Rules',
         meta:{
             web:true
         } 
