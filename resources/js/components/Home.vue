f433f2c5fb8938fa5f4273de9bb88f3f1d41eef2
Temuchin Torchenuk <temuchin26@gmail.com>
2019-02-22 18:01:32 +0200

    fix scroll

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -181,7 +181,7 @@
                                 <span class="page-5-star-name"> {{ star.name }} {{ star.surname }} </span>
                                 <span class="page-5-star-city">м. {{ star.city }}</span>
                             </div>
-                            <div class="page-5-star desktop-only" v-if="stars.length < 1" @click="goToStar()">
+                            <!-- <div class="page-5-star desktop-only" v-if="stars.length < 1" @click="goToStar()">
                                 <img class="page-5-star-photo" src="img/ladies/01.jpg"   alt="avatar">
                             </div>
                             <div class="page-5-star desktop-only" v-if="stars.length < 1" @click="goToStar()">
@@ -207,7 +207,7 @@
                                 </swiper-slide>
                             </swiper>
                             <div class="swiper-button-prev" slot="button-prev"></div>
-                            <div class="swiper-button-next" slot="button-next"></div>
+                            <div class="swiper-button-next" slot="button-next"></div> -->
                         </div>
                         <v-btn
                             text="До галереї"
